3711a050a7859f7fc9d514780cdd1f59345a24fe
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-02-18 19:08:54 -0300

    Add files via upload

---
+++ A.bit.at/src/main/java/com/javamaster/dao/impl/ConnectionPool.java
@@ -0,0 +1,66 @@
+package com.javamaster.dao.impl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionPool {
+	
+	
+	private ConnectionPool(){
+		//private constructor
+	}
+	
+	/**
+	 * Connection instance
+	 */
+	private Connection connection = getConnection();
+
+	private static ConnectionPool instance = null;
+	
+	public static ConnectionPool getInstance(){
+		if (instance==null)
+			instance = new ConnectionPool();
+		return instance;
+	}
+	
+	/**
+	 * Getting connection from connection pool.
+	 *
+	 * @see ConnectionPool
+	 * @throws SQLException
+	 */
+	Connection getConnection() {
+
+				try {
+					Class.forName("org.postgresql.Driver").newInstance();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		
+		String username = "oflcjyacboqwij";
+		String password = "2216fd15038c51df6f8d713a73bded0d5d4d7d790ba3297463c6cbddeccf988c";
+		String dbUrl = "jdbc:postgresql://" + "ec2-50-19-86-139.compute-1.amazonaws.com"
+				+ "/daa5a9asfvd28v?sslmode=require";
+		try {
+			return DriverManager.getConnection(dbUrl, username, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+}
